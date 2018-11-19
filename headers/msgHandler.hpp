5094001b3bf8960393b00cb28980b6b52bdfa221
tenfoldpaper <tenfoldpaper@gmail.com>
2018-11-19 14:38:34 +0100

    Hello?

--- headers/msgHandler.hpp
+++ headers/msgHandler.hpp
@@ -1,27 +1,42 @@
-//Header file for the message handler component of the HUB. 
-//
+
+/*
+ * The message handler is a unidirectional message conveyer. 
+ * It continuously listens to the fromPort, and then conveys the message 
+ * to the other side. It also handles the INSERT function of the local 
+ * SQLite database. 
+ *   
+ */
+#ifndef MSGHANDLER_HPP
+#define MSGHANDLER_HPP
 
 #include <iostream>
 #include <vector>
 #include <string> 
 #include <stdio.h>
 #include <string>
+#include <zmq.hpp>
+#include "./headers/zhelpers.hpp"
 
 class msgHandler {
 
 private:
     std::vector<std::string> msgQueue; 
-    int portNumber; //Specifies the port the msgHandler receives data from 
+    int toPort; //Specifies the port the msgHander sends the data to 
+    int fromPort; //Specifies the port the msgHandler receives data from 
     bool empty; 
+    zmq::socket_t* fromSocket; 
+    zmq::socket_t* heartSocket;
+    zmq::socket_t* toSocket; 
+    bool direction; //0: to Sat; 1: to CCS
 
 public:
     msgHandler();
 
-    msgHandler(int);
+    msgHandler(int, zmq::socket_t*, zmq::socket_t*, zmq::socket_t*);
 
     ~msgHandler();
     
-    bool receiveMsg(std::string);//Pushes the msg in the arg to its internal queue
+    bool insertMsg(std::string);//Pushes the msg in the arg to SQLite db 
     
     bool handleMsg();//Goes through the entire queue, storing it into the database. 
 
@@ -34,3 +49,5 @@ public:
     void setEmpty(bool);
     
 };
+
+#endif 
